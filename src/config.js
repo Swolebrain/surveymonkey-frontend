1e1a09e21ee0f046569ac586d3067c7312ceb68e
Swolebrain <thecodingteacher@gmail.com>
2017-10-05 15:50:58 -0400

    changed to allow https support

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 
 
-const API_URI = window.location.hostname==="localhost" ? "http://localhost:1337/api" : "http://api.techlaunch.io:1337/api";
+const API_URI = window.location.hostname==="localhost" ? 
+"http://localhost:1337/api" : "https://api.techlaunch.io:1337/api";
 export {API_URI};
